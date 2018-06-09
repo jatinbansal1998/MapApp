e316c555f386fa3ac1ecfb853d93ff19ca0cd2a5
jatin_bansal <bansal.jatin1998@gmail.com>
2018-06-09 16:33:08 +0530

    added cocapods google maps dependency

---
+++ MapApp/PlaceSuggestionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaceSuggestionViewController.swift
+//  MapApp
+//
+//  Created by Jatin Bansal on 09/06/18.
+//  Copyright © 2018 Jatin Bansal. All rights reserved.
+//
+
+import UIKit
+
+class PlaceSuggestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
